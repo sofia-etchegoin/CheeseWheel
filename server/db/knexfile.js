10d5c8b4b5414cbeae63a762cabe02868c9b4071
Sofia Etchegoin <sofiaetchegoin@gmail.com>
2023-12-18 11:05:33 +1300

    Ran migrations and seed file

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -1,3 +1,4 @@
+//knexfile.js
 import * as Path from 'node:path'
 import * as URL from 'node:url'
 
---
+++ server/db/migrations/20231217215847_cheese.js
@@ -0,0 +1,15 @@
+export function up(knex) {
+  return knex.schema.createTable('cheese', (table) => {
+    table.increments('id')
+    table.integer('cheese_box')
+    table.varchar('name')
+    table.varchar('maker')
+    table.varchar('description')
+    table.varchar('region')
+    table.varchar('type_of_milk')
+  })
+}
+
+export function down(knex) {
+  return knex.schema.dropTable('cheese')
+}
